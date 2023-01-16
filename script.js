31939783b16132321b0a6c0ab763d04bc8053f5e
SimonyanR <SimonyanRR@gmail.com>
2023-01-16 23:54:55 +0200

    water added

--- script.js
+++ script.js
@@ -7,6 +7,41 @@ function coordinateRandomizer(max) {
   return result;
 }
 
+function drawTile(canvas, x, y, tile) {
+  let bound = canvas.length;
+  if (y < 0 || x < 0 || x > bound - 1 || y > bound - 1) return;
+  canvas[x][y] = tile;
+}
+
+function checkBounds(canvas, x, y) {
+  let bound = canvas.length;
+  return !(y < 0 || x < 0 || x > bound - 1 || y > bound - 1);
+}
+
+function spawnCharacter(waterMap, name, type) {
+  let character = new Character(
+    name,
+    coordinateRandomizer(mapSize),
+    coordinateRandomizer(mapSize),
+    type
+  );
+  while (waterMap[character.row][character.column] !== undefined) {
+    character.row = coordinateRandomizer(mapSize);
+    character.column = coordinateRandomizer(mapSize);
+  }
+
+  return character;
+}
+
+function respawnCharacter(waterMap, character, target) {
+  if (character.row === target.row && character.column === target.column) {
+    do {
+      target.row = coordinateRandomizer(mapSize);
+      target.column = coordinateRandomizer(mapSize);
+    } while (waterMap[target.row][target.column] !== undefined);
+  }
+}
+
 function arrayGenerator(mapSize) {
   let array = [];
   for (var i = 0; i < mapSize; i++) {
@@ -15,20 +50,78 @@ function arrayGenerator(mapSize) {
   return array;
 }
 
-function mapGenerator(mapSize) {
-  let map = arrayGenerator(mapSize);
+function backgroundGenerator(mapSize) {
+  let background = arrayGenerator(mapSize);
 
   for (let i = 0; i < mapSize; i++) {
     for (let j = 0; j < mapSize; j++) {
-      let random = Math.floor(Math.random() * floor.length);
-      map[i][j] = floor[random];
+      background[i][j] = backgroundTile;
     }
   }
 
-  return map;
+  return background;
 }
 
-async function mapRender(map, heroes) {
+function waterGenerator(mapSize) {
+  let water = arrayGenerator(mapSize);
+  let x0 = coordinateRandomizer(mapSize);
+  let y0 = coordinateRandomizer(mapSize);
+
+  water[x0][y0] = waterTile;
+
+  let r = 3;
+  let x = r;
+  let y = 0;
+  let error = 1 - x;
+
+  while (x >= y) {
+    drawTile(water, x + x0, y + y0, waterTile);
+    drawTile(water, y + x0, x + y0, waterTile);
+    drawTile(water, -x + x0, y + y0, waterTile);
+    drawTile(water, -y + x0, x + y0, waterTile);
+    drawTile(water, -x + x0, -y + y0, waterTile);
+    drawTile(water, -y + x0, -x + y0, waterTile);
+    drawTile(water, x + x0, -y + y0, waterTile);
+    drawTile(water, y + x0, -x + y0, waterTile);
+    y++;
+
+    if (error < 0) {
+      error += 2 * y + 1;
+    } else {
+      x--;
+      error += 2 * (y - x + 1);
+    }
+  }
+
+  let border = [{ x: x0, y: y0 }];
+  let filled = [];
+
+  while (border.length !== 0) {
+    let i = 0;
+    console.log(border[i]);
+    let x = border[i].x;
+    let y = border[i].y;
+    if (checkBounds(water, x + 1, y) && water[x + 1][y] !== waterTile) {
+      border.push({ x: x + 1, y: y });
+    }
+    if (checkBounds(water, x - 1, y) && water[x - 1][y] !== waterTile) {
+      border.push({ x: x - 1, y: y });
+    }
+    if (checkBounds(water, x, y + 1) && water[x][y + 1] !== waterTile) {
+      border.push({ x: x, y: y + 1 });
+    }
+    if (checkBounds(water, x, y - 1) && water[x][y - 1] !== waterTile) {
+      border.push({ x: x, y: y - 1 });
+    }
+    water[x][y] = waterTile;
+    filled.push(border.shift());
+  }
+
+  return water;
+}
+
+async function mapRender(map, waterMap, characters) {
+  // Generating canvas
   let canvas = arrayGenerator(map.length);
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map.length; j++) {
@@ -36,11 +129,20 @@ async function mapRender(map, heroes) {
     }
   }
 
-  for (const i in heroes) {
-    const hero = heroes[i];
-    canvas[hero.row][hero.column] = hero.character;
+  // Adding water map to canvas
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map.length; j++) {
+      if (waterMap[i][j] !== undefined) canvas[i][j] = waterMap[i][j];
+    }
   }
 
+  //Characters spawn
+  for (const i in characters) {
+    const character = characters[i];
+    canvas[character.row][character.column] = character.type;
+  }
+
+  // Render map in HTML
   let indexCanvas = document.getElementById("canvas");
   let renderedMap = "";
 
@@ -54,60 +156,54 @@ async function mapRender(map, heroes) {
   indexCanvas.innerHTML = renderedMap;
 }
 
-class Hero {
-  constructor(r, c, character) {
+function followTarget(character, target) {
+  if (target.row !== character.row)
+    target.row > character.row ? character.row++ : character.row--;
+  if (target.column !== character.column)
+    target.column > character.column ? character.column++ : character.column--;
+}
+
+class Character {
+  constructor(name, r, c, type) {
+    this.name = name;
     this.row = r;
     this.column = c;
-    this.character = character;
+    this.type = type;
   }
 }
 
-function followHero(hero, target) {
-  if (target.row !== hero.row) target.row > hero.row ? hero.row++ : hero.row--;
-  if (target.column !== hero.column)
-    target.column > hero.column ? hero.column++ : hero.column--;
-}
-
-function checkPosition(hero, target, mapSize) {
-  if (target.row === hero.row && target.column === hero.column) {
-    target.row = Math.floor(Math.random() * mapSize);
-    target.column = Math.floor(Math.random() * mapSize);
+class Structure {
+  constructor(r, c, type) {
+    this.row = r;
+    this.column = c;
+    this.type = type;
   }
 }
 
 const mapSize = 10;
-const floor = ["🟩", "🟫"];
+const backgroundTile = "🟩";
+const waterTile = "🌊";
 
 async function update() {
-  let dog = new Hero(
-    coordinateRandomizer(mapSize),
-    coordinateRandomizer(mapSize),
-    "🐕"
-  );
-  let cat = new Hero(
-    coordinateRandomizer(mapSize),
-    coordinateRandomizer(mapSize),
-    "🐈"
-  );
-  let roma = new Hero(
-    coordinateRandomizer(mapSize),
-    coordinateRandomizer(mapSize),
-    "😤"
-  );
-  let map = mapGenerator(mapSize);
-  let heroes = [dog, roma, cat];
+  // Generate water map
+  const waterMap = waterGenerator(mapSize);
 
+  let dog = spawnCharacter(waterMap, "Dog", "🐕");
+  let cat = spawnCharacter(waterMap, "Cat", "🐈");
+
+  let map = backgroundGenerator(mapSize);
+  let heroes = [dog, cat];
   let frame = 0;
 
   while (true) {
     // Game logic
-    followHero(dog, cat);
+    followTarget(dog, cat);
 
-    if (frame % 1.5 === 0) followHero(roma, dog);
-    checkPosition(dog, cat, mapSize);
+    // if (frame % 1.5 === 0) followHero(roma, dog);
+    respawnCharacter(waterMap, dog, cat);
 
     // Render
-    mapRender(map, heroes);
+    mapRender(map, waterMap, heroes);
     await sleep(500);
 
     frame++;
